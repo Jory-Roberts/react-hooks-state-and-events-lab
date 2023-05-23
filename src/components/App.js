9e739ed6dc58e5bdb8f61fd5b733bbd705a23742
Jory Roberts <robertsjory13@gmail.com>
2023-05-23 06:21:09 -0500

    adds toggle functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
-import React from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React, { useState } from 'react';
+import ShoppingList from './ShoppingList';
+import itemData from '../data/items';
 
 function App() {
+    const [toggle, setToggle] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+    // replace 'false' with a state variable that can be toggled between true and false
+    // this will be used for the Dark Mode Toggle feature
+    const appClass = toggle ? 'App dark' : 'App light';
 
-  return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+    return (
+        <div className={appClass}>
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={() => setToggle(!toggle)}>Dark Mode</button>
+            </header>
+            <ShoppingList items={itemData} />
+        </div>
+    );
 }
 
 export default App;
